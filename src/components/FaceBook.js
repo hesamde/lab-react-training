162eb352e362d93cb306f794efc38ecdc81431e3
hesam dehghanmoshtaghin <hdm.hesam@gmail.com>
2023-08-03 18:53:26 +0200

    Iteration 13-update css

--- src/components/FaceBook.js
+++ src/components/FaceBook.js
@@ -4,13 +4,12 @@ import profiles from './../data/berlin.json';
     return (
         <div class="facebook grid">{profiles.map((profile, i) => (
 
-            <article>
+            <article className='design-f'>
             <img src={profile.img} alt={(profile.firstName, profile.lastName)}width="200" height="200"/>
             <p>
-                <strong>Name:</strong> {profile.firstName} {profile.lastName}
-            <br />
-                <strong>Country:</strong> {profile.country}
-            <br />
+                <strong>First name:</strong> {profile.firstName}<br />
+                <strong>Lasy name:</strong> {profile.lastName}<br />
+                <strong>Country:</strong> {profile.country}<br />
                 <strong>Type:</strong> {profile.isStudent ? 'Student' : 'Teacher'}
             </p>
             </article>
