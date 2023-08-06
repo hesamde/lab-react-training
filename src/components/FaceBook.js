e8784efcb0f703c2c97dc57e8254fb21b4ead506
hesam dehghanmoshtaghin <hdm.hesam@gmail.com>
2023-08-06 20:16:57 +0200

    Iteration 13-14

--- src/components/FaceBook.js
+++ src/components/FaceBook.js
@@ -1,21 +1,73 @@
-import profiles from './../data/berlin.json';
+import React, { useState } from 'react';
+import profilesData from '../data/berlin.json';
 
-    function FaceBook() {
+const FaceBook = () => {
+    const [profileList, setProfileList] = useState(profilesData);
+    const [selectedCountry, setSelectedCountry] = useState(null);
+
+    const selectCountry = (country) => {
+        setSelectedCountry(country);
+    };
+
+    const studentsProfile = profilesData.filter((student) => student.isStudent);
+    const countriesOfStudents = Array.from(new Set(studentsProfile.map((student) => student.country)));
+
+    const getSorting = (value) => {
+        const copy = [...profileList];
+        if (value === 'alphabeticalAscending') {
+        copy.sort((a, b) => a.lastName.localeCompare(b.lastName));
+        } else if (value === 'alphabeticalDescending') {
+        copy.sort((a, b) => b.lastName.localeCompare(a.lastName));
+        }
+        setProfileList(copy);
+    };
+
     return (
-        <div class="facebook grid">{profiles.map((profile, i) => (
+        <div>
+            <button
+            onClick={() => {
+            setProfileList(profilesData);
+            setSelectedCountry(null);}}>All
+            </button>
 
-            <article className='design-f'>
-            <img src={profile.img} alt={(profile.firstName, profile.lastName)}width="200" height="200"/>
-            <p>
-                <strong>First name:</strong> {profile.firstName}<br />
-                <strong>Lasy name:</strong> {profile.lastName}<br />
-                <strong>Country:</strong> {profile.country}<br />
-                <strong>Type:</strong> {profile.isStudent ? 'Student' : 'Teacher'}
-            </p>
-            </article>
+        {countriesOfStudents.map((country) => (
+            <button key={country} onClick={() => selectCountry(country)}>{country}</button>
         ))}
+
+        <div>
+            <label htmlFor="profile-sort">Sort: </label>
+            <select onChange={(event) => getSorting(event.target.value)} name="profile" id="profile-sort">
+            <option value="default">Default</option>
+            <option value="alphabeticalAscending">Name: A-Z</option>
+            <option value="alphabeticalDescending">Name: Z-A</option>
+            </select>
+        </div>
+
+        <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+            {profileList.map((profile) => (
+            <div
+                key={profile.firstName + profile.lastName}
+                className="card col-3"style={{backgroundColor: profile.country === selectedCountry ? 'lightblue' : 'white',border: 'solid',}}>
+                <img className="head-shot" src={profile.img} alt="" />
+                <div className="card-content">
+                <p>
+                    <span>First name: </span>{profile.firstName}
+                </p>
+                <p>
+                    <span>Last name: </span>{profile.lastName}
+                </p>
+                <p>
+                    <span>Country: </span>{profile.country}
+                </p>
+                <p>
+                    <span>Type: </span>{profile.isStudent ? 'Student' : 'Teacher'}
+                </p>
+                </div>
+            </div>
+            ))}
+        </div>
         </div>
     );
-    }
+    };
 
 export default FaceBook;
